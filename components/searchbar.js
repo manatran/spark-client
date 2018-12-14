fe12ceb87c46058214418207f7de40e4f3a3fc8c
Manaus Transez <manatran@student.arteveldehs.be>
2018-12-14 14:56:47 +0100

    Edited SearchBar component

--- components/searchbar.js
+++ components/searchbar.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight
+} from "react-native";
 
 export default class SearchBar extends React.Component {
   constructor(props) {
@@ -14,18 +20,23 @@ export default class SearchBar extends React.Component {
     console.log(term);
   }
 
-  onFocus(){
-    this.setState({focus: true})
-  }
+  // onFocus() {
+  //   this.setState({ focus: true });
+  // }
 
-  onBlur(){
-    console.log('blur')
+  // onBlur() {
+  //   this.setState({ focus: false });
+  // }
+
+  toggleClass() {
+    console.log("clicked");
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
+          <View style={styles.searchIcon} />
           <TextInput
             placeholder="Search here..."
             value={this.state.term}
@@ -34,6 +45,13 @@ export default class SearchBar extends React.Component {
             onFocus={this.onFocus}
             onBlur={this.onBlur}
           />
+          <View style={[styles.searchIcon, styles.voiceIcon]} />
+          <TouchableHighlight
+            onPress={this.toggleClass}
+            style={[styles.voiceButton]}
+          >
+            <View style={[styles.searchIcon, styles.voiceIcon]} />
+          </TouchableHighlight>
         </View>
       </View>
     );
@@ -46,15 +64,46 @@ const styles = StyleSheet.create({
     marginTop: 32
   },
   inputContainer: {
+    position: "relative",
     backgroundColor: "white",
-    padding: 16,
+    paddingHorizontal: 16,
+    height: 49,
     borderRadius: 50,
-    elevation: 1
+    elevation: 1,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  searchIcon: {
+    height: 16,
+    width: 16,
+    marginRight: 32,
+    backgroundColor: "#BEBEBE"
+  },
+  voiceButton: {
+    backgroundColor: "white",
+    position: "absolute",
+    right: 0,
+    height: 49,
+    width: 49,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  voiceIcon: {
+    position: "absolute",
+    right: 16,
+    marginRight: 0
+  },
+  voiceActive: {
+    backgroundColor: "#61D0E1",
+    elevation: 2,
+    left: 0
   },
   input: {
     fontSize: 14,
     color: "#8A8A8A",
     fontWeight: "bold",
-    padding: 16
+    padding: 16,
+    flex: 1
   }
 });
