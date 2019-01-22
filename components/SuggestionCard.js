0357eb38937b2b37102db5a50b145ae8bbcbb0d3
Yensbroo <yensbroothaers@gmail.com>
2019-01-22 16:03:58 +0100

    Add dynamic props from history to suggestion cards

--- components/SuggestionCard.js
+++ components/SuggestionCard.js
@@ -18,10 +18,8 @@ class SuggestionCard extends React.Component {
     return (
       <TouchableHighlight
         onPress={this.onPress.bind(this)}
-        style={[
-          this.props.roundTop ? styles.roundTop : null,
-          this.props.roundBottom ? styles.roundBottom : null
-        ]}>
+        style={[this.props.roundTop ? styles.roundTop : null, this.props.roundBottom ? styles.roundBottom : null]}
+      >
         <View
           style={[
             styles.container,
@@ -32,17 +30,11 @@ class SuggestionCard extends React.Component {
           <Icon icon={this.props.icon} style={styles.icon} />
           <View style={{ flex: 1 }}>
             {/* Title */}
-            {this.props.title ? (
-              <Text style={styles.title}>{this.props.title}</Text>
-            ) : null}
+            {this.props.title ? <Text style={styles.title}>{this.props.title}</Text> : null}
             {/* Subtitle */}
-            {this.props.subtitle ? (
-              <Text style={styles.subtitle}>{this.props.subtitle}</Text>
-            ) : null}
+            {this.props.subtitle ? <Text style={styles.subtitle}>{this.props.subtitle}</Text> : null}
           </View>
-          {this.props.button ? (
-            <Icon icon="directions" style={styles.button} />
-          ) : null}
+          {this.props.button ? <Icon icon="directions" style={styles.button} /> : null}
         </View>
       </TouchableHighlight>
     );
@@ -58,7 +50,7 @@ export default connect(
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingHorizontal: 16,
     paddingVertical: 8,
     flexDirection: "row",
@@ -68,17 +60,17 @@ const styles = StyleSheet.create({
   },
   roundTop: {
     borderTopStartRadius: 8,
-    borderTopEndRadius: 8,
+    borderTopEndRadius: 8
   },
   roundBottom: {
     borderBottomStartRadius: 8,
     borderBottomEndRadius: 8,
-    borderBottomWidth: 0,
+    borderBottomWidth: 0
   },
   icon: {
     color: "#A1A1A1",
     marginRight: 16,
-    fontSize: 20,
+    fontSize: 20
   },
   button: {
     borderRadius: 50,
@@ -87,16 +79,16 @@ const styles = StyleSheet.create({
     // color: "white",
     // backgroundColor: "#61D0E1",
     fontSize: 24,
-    color: "#61D0E1",
+    color: "#61D0E1"
   },
   title: {
     fontWeight: "700",
     fontSize: 16,
-    color: "#303030",
+    color: "#303030"
   },
   subtitle: {
     fontWeight: "300",
     fontSize: 14,
-    color: "#303030",
+    color: "#303030"
   }
 });
